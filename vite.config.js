c6dfb83bf4388b74aa0fab516a5c827ba553a774
Elysa23 <121961510+Elysa23@users.noreply.github.com>
2025-05-14 07:17:26 +0200

    Update vite.config.js forcer génération URL en HTTPS

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-
-    base: '/build/',
+    base: 'https://testdeploiement-production.up.railway.app/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -11,6 +10,3 @@ export default defineConfig({
         }),
     ],
 });
-
-
-
